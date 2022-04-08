94362b0fd17f7c94d8630fac8252599f6c9c0642
NWhaley <nicholas.whaley@gmail.com>
2022-04-07 17:56:47 -0700

    set up clock and state

--- interval-clock/src/components/clock.jsx
+++ interval-clock/src/components/clock.jsx
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
-class Clock extends Component {
-    state = { 
-        timer : 0
-     } 
-    render() { 
+function Clock(){
+
+    const [active, setActive] = useState(false)
+    const [time, setTime] = useState(10)
+
+    //work on useEffect and setInterval integration
+    useEffect(() => {
+        let timer = setInterval(() => {
+            setTime(time--)
+        }, 1000);
+        return () => clearTimeout(timer)
+    }, [])
+
+        if(time <= 0){
+            return (
+                <div>Time's up!</div>
+            )
+        }
+    
+        
+
         return (
 
-            <div><h1>{this.state.timer}</h1></div>
+            <div>
+                <h1>{time}</h1>
+                <button onClick={() => setActive(!active)}>Start</button>
+                <h2>{active.toString()}</h2>
+            </div>
         );
     }
-}
+
  
 export default Clock;
